26159a832b5d3939c4e685d8f5df39ac842b0763
Robert Rees <spam@shudderfix.com>
2011-04-17 13:41:41 +0100

    Checking the Leiningen actually works

--- project.clj
+++ project.clj
@@ -1,5 +1,6 @@
 (defproject coloured-balls "0.0.1-SNAPSHOT"
   :description "A visual game challenge for the Clojure dojo"
+  :main coloured-balls.core
   :dependencies [[org.clojure/clojure "1.2.0"]
 		[midje "1.1-alpha-3"]
 		[org.clojars.automata/rosado.processing "1.1.0"]])
